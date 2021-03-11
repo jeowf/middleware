cd88938665720b8f372ad9b0693bce8329bd1f57
Alison Hedigliranes <alison.hedigliranes@gmail.com>
2021-03-11 19:48:23 -0300

    20 anos de curso e n sei usar git

--- middleware/src/main/java/application/UserProxy.java
+++ middleware/src/main/java/application/UserProxy.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.io.IOException;
+
 import basic.RemoteError;
 import basic.client.ClientProxy;
 import basic.client.ClientRequestHandler;
@@ -7,25 +9,60 @@ import basic.client.Requestor;
 
 public class UserProxy extends ClientProxy{
 
-	private long realID;
-	private Requestor requestor;
-	private ClientRequestHandler clientRequestHandler;
-	
-	public UserProxy() {
-		this.clientRequestHandler = new ClientRequestHandler();
-		this.requestor = new Requestor(clientRequestHandler, User.class);
+	//Lembrar de revisar se ainda vamos precisar no final
+	public UserProxy(String nome, String senha, Double saldo, Integer numAcc) {
+		super(User.class);
 		
 		try {
-			//System.out.println(requestor.invoke(-1, "*constructor" , null, null));
+			System.out.println(requestor.invoke(-1, "*constructor" , null, null));
 			
-			Long id = (long) Math.floor((Double) requestor.invoke(-1, "*constructor" , null, null));
+			Long id = (long) Math.floor((Double) requestor.invoke(-1, "*constructor" , 
+					new Object[]{nome,
+							senha, 
+							saldo,
+							numAcc} ,
+					new String[]{nome.getClass().getName(),
+							senha.getClass().getName(),
+							saldo.getClass().getName(),
+							numAcc.getClass().getName()}));
 			System.out.println("ID DO MENINO " + id);
 			this.realID = id;
 		} catch (RemoteError e) {
-			// TODO Auto-generated catch block
+		    // TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
+//	public UserProxy() {
+//		this.clientRequestHandler = new ClientRequestHandler();
+//		this.requestor = new Requestor(clientRequestHandler, User.class);
+//		
+//		//try {
+//			//System.out.println(requestor.invoke(-1, "*constructor" , null, null));
+//			
+//			//Long id = (long) Math.floor((Double) requestor.invoke(-1, "*constructor" , null, null));
+//			//System.out.println("ID DO MENINO " + id);
+//			//this.realID = id;
+//		//} catch (RemoteError e) {
+//			// TODO Auto-generated catch block
+//		//	e.printStackTrace();
+//		//}
+//	}
+	
+	// Fun��o que ir� consultar um objeto na lookup e retornar seu ID
+	public void requestAOR(String objType) throws IOException
+	{
+		try {
+			// Enviamos a mensagem com o c�digo -2 (lookup message) para o invoker, esperando receber o ID do objeto
+			Long id = (long) Math.floor((Double) requestor.invoke(-2, objType , null, null));
+			System.out.println("ID DO MENINO " + id);
+			this.realID = id;
+		} catch (RemoteError e) {
+			throw new IOException("Erro ao obter o ID do objeto requisitado");
+		}
+	}
+	
+	// TODO fazer um requisitor gen�rico.
 	
 	public String getNome() {
 		
@@ -38,6 +75,7 @@ public class UserProxy extends ClientProxy{
 		
 		return null;
 	}
+	
 	public void setNome(String nome) {
 		try {
 			requestor.invoke(realID, "setNome" ,
@@ -48,22 +86,71 @@ public class UserProxy extends ClientProxy{
 			e.printStackTrace();
 		}
 	}
-//	public String getSenha() {
-//		return senha;
-//	}
-//	public void setSenha(String senha) {
-//		this.senha = senha;
-//	}
-//	public Double getSaldo() {
-//		return saldo;
-//	}
-//	public void setSaldo(Double saldo) {
-//		this.saldo = saldo;
-//	}
-//	public Integer getNumAcc() {
-//		return numAcc;
-//	}
-//	public void setNumAcc(Integer numAcc) {
-//		this.numAcc = numAcc;
-//	}
+	
+	public String getSenha() {
+		
+		try {
+			return (String) requestor.invoke(realID, "getSenha" , null, null);
+		} catch (RemoteError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public void setSenha(String senha) {
+		try {
+			requestor.invoke(realID, "setSenha" ,
+					new Object[]{senha} ,
+					new String[]{senha.getClass().getName()});
+		} catch (RemoteError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public Double getSaldo() {
+		try {
+			return (Double) requestor.invoke(realID, "getSaldo" , null, null);
+		} catch (RemoteError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public void setSaldo(Double saldo) {
+		try {
+			requestor.invoke(realID, "setSaldo" ,
+					new Object[]{saldo} ,
+					new String[]{saldo.getClass().getName()});
+		} catch (RemoteError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public Integer getNumAcc() {
+		try {
+			return (Integer) requestor.invoke(realID, "getNumAcc" , null, null);
+		} catch (RemoteError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public void setNumAcc(Integer numAcc) {
+		try {
+			requestor.invoke(realID, "setNumAcc" ,
+					new Object[]{numAcc} ,
+					new String[]{numAcc.getClass().getName()});
+		} catch (RemoteError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
