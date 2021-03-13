fae37858de393116c32da2a453e8c823b45a0273
Giovanne <giovannedssantos@gmail.com>
2021-03-13 12:37:36 -0300

    staticInstance create instance

--- middleware/src/main/java/lifecycle/staticInstance.java
+++ middleware/src/main/java/lifecycle/staticInstance.java
@@ -3,6 +3,7 @@ package lifecycle;
 
 import basic.Marshaller;
 import basic.RemoteError;
+import basic.server.InstanceList;
 import basic.server.Invoker;
 import basic.server.InvokerRegistry;
 import general.LookUpMessage;
@@ -11,12 +12,17 @@ import identification.lookup.*;
 
 public class staticInstance {
 	Invoker staticInvoker; 
+	InstanceList instanceList; 
 	
 	public Invoker getStaticInvoker() {
 		return staticInvoker;
 	}
 	
-	public void registerStaticInstance() {
+	public void createStaticInstance(Class<?> cls, Object obj) throws RemoteError {
+		instanceList.addInstance(cls, obj);
+	}
+	
+	public void destroyStaticInstance(long id) {
 		
 	}
 	
