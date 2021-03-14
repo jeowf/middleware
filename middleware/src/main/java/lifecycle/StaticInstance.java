675ef660240dca27898dfa523be58240cd333a6d
Giovanne <giovannedssantos@gmail.com>
2021-03-14 11:45:35 -0300

    static feito

--- middleware/src/main/java/lifecycle/StaticInstance.java
+++ middleware/src/main/java/lifecycle/StaticInstance.java
@@ -4,12 +4,20 @@ package lifecycle;
 import java.util.HashMap;
 import java.util.Set;
 
+import basic.Marshaller;
 import basic.RemoteError;
-
+import basic.server.Invoker;
+import basic.server.InvokerRegistry;
+import general.InvocationData;
+import general.LookUpMessage;
+import general.RequestorMessage;
 import identification.lookup.*;
 
 public class StaticInstance {
 	LookUp lookUp = new LookUp();
+	Invoker invoker = new Invoker(30);
+
+	Marshaller marshaller = new Marshaller();
 	HashMap<String,Long> staticObjectsReferences = new HashMap<String, Long>(10);
 	
 	public HashMap<String,Long> getStaticObjectsReferences() {
@@ -29,12 +37,26 @@ public class StaticInstance {
 	}
 	
 	public void create() throws RemoteError {
+		long id = 0;
 		for(int i = 0; i < 10 ; i++) {
 			String nameObject = "objeto" + String.valueOf(i);
 			print(nameObject);
 			
-			Long id = lookUp.lookup(nameObject);
-			staticObjectsReferences.put(nameObject,id);
+			InvocationData staticInvocationData = new InvocationData(id,"*constructor",null,null,String.class.getName());
+			id++;
+			RequestorMessage rm = new RequestorMessage(0,staticInvocationData);
+			String staticMessage = marshaller.marshal(rm);
+			
+			
+			Long idObj = (Long)invoker.invoke(staticMessage);
+			LookUpMessage lm = new LookUpMessage(idObj,String.class.getName(),staticInvocationData);
+			staticMessage = marshaller.marshal(lm);
+			lookUp.lookup(staticMessage);
+			
+			
+			staticObjectsReferences.put(nameObject,idObj);
+			
+			
 		}
 		
 		printHash();
