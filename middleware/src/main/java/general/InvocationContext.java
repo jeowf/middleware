e438fdf1a3be056b4c3e12d9612c0798fb4cb810
bruno <bczion@hotmail.com>
2021-03-14 20:46:46 -0300

    Criação do InvocationContext para 'setar' dados de contexto ao servidor.

---
+++ middleware/src/main/java/general/InvocationContext.java
@@ -0,0 +1,24 @@
+package general;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import patterns.strategy.ClientInterceptorStrategy;
+
+public class InvocationContext {
+	
+	Map<String, List<ClientInterceptorStrategy>> interceptors;
+	
+	public InvocationContext() {
+		
+		interceptors = new HashMap<String, List<ClientInterceptorStrategy>>();
+		
+	}
+
+	public void setInterceptors(Map<String, List<ClientInterceptorStrategy>> interceptorsReady) {
+		this.interceptors = interceptorsReady;
+		
+	}
+
+}
