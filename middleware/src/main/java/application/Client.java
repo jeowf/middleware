03dddd5a8855e3f020a9701a5231b38dc13fc38f
bruno <bczion@hotmail.com>
2021-03-20 18:18:39 -0300

    To Merge

--- middleware/src/main/java/application/Client.java
+++ middleware/src/main/java/application/Client.java
@@ -21,6 +21,50 @@ public class Client {
 		return null;
 		
 	}
+	
+	public void test() {
+		System.out.println( "Teste" );
+		/*
+		// Construindo o interceptor para log
+		InterceptorStrategy clientInterceptorLog = genderInterceptor( InterceptorType.INTERCEPTOR_LOG );
+		clientInterceptorLog.buildInterceptor();
+		
+		// Registrando o interceptor na lista
+		//InterceptorRegistry interceptorRegistry = new InterceptorRegistry();
+		//interceptorRegistry.addInterceptor( clientInterceptorLog );
+		InterceptorRegistryClient.addInterceptor( (LogClientInterceptor) clientInterceptorLog );
+		
+		ClientRequestHandler crh = new ClientRequestHandler();
+		
+		Requestor r = new Requestor(crh, String.class);
+		//r.invoke(id, methodName, args, argsTypes)
+		/*
+		UserProxy up = new UserProxy("RC", "123", 150.0, 12345678);
+		
+		up.setNome("Kevin Corno");
+		
+		System.out.println("Nome: " + up.getNome());
+		
+		up.setNome("RC");
+		
+		System.out.println("Nome: " + up.getNome());
+		
+		//System.out.println(up.getNome().);
+
+
+		
+		try {
+			System.out.println((String) r.invoke(0, "equals",
+					new Object[]{"pão"}, 
+
+					new String[] {Object.class.getName()}));
+		} catch (RemoteError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		*/
+	}
 
 	public static void main(String[] args) {
 		
@@ -36,7 +80,8 @@ public class Client {
 		ClientRequestHandler crh = new ClientRequestHandler();
 		
 		Requestor r = new Requestor(crh, String.class);
-		
+		//r.invoke(id, methodName, args, argsTypes)
+		/*
 		UserProxy up = new UserProxy("RC", "123", 150.0, 12345678);
 		
 		up.setNome("Kevin Corno");
@@ -48,18 +93,18 @@ public class Client {
 		System.out.println("Nome: " + up.getNome());
 		
 		//System.out.println(up.getNome().);
+*/
 
+		
+		try {
+			System.out.println((String) r.invoke(0, "equals",
+					new Object[]{"pão"}, 
 
-//		
-//		try {
-//			System.out.println((String) r.invoke(0, "equals",
-//					new Object[]{"pão"}, 
-//
-//					new String[] {Object.class.getName()}));
-//		} catch (RemoteError e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+					new String[] {Object.class.getName()}));
+		} catch (RemoteError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 
